f5a522f0d32eee560eb376cf41c70455df2c1659
Ramon1976 <38327068+Ramon1976@users.noreply.github.com>
2018-04-13 18:04:55 +0200

    Initial release

---
+++ dist/bootstrap-textareacounter.js
@@ -0,0 +1,33 @@
+﻿/*!
+ * Textarea with inner character counter
+ *
+ * Copyright 2018 Teddy Limited
+ * http://www.teddyltd.com
+ *
+ * Licensed under the Apache License v2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ */
+
+    //Default constructor
+    $.fn.TextareaCounter = function () {
+
+        $(this).each(function(index, element) {
+            var tags = "<span class='pull-right label label-default text-area-counter-message' ></span>";
+            var counter = $(tags);
+            $(element).css("resize", "vertical");
+            $(element).after(counter);
+
+            var text_max = $(element).attr("maxLength") || 255;
+            
+            counter.html('0 / ' + text_max);
+
+            $(element).keyup(function () {
+                var text_length = $(this).val().length;
+                var text_remaining = text_max - text_length;
+
+                counter.html(text_length + ' / ' + text_max);
+            });
+        });
+    }
+
